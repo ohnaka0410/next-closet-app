990b95a0d244aa31015bf85df085b16eabfc51b1
yuki0410_ <>
2021-08-29 15:21:55 +0900

    型定義修正

--- src/@types/Item.d.ts
+++ src/@types/Item.d.ts
@@ -3,12 +3,13 @@ export type Item = {
   genreKey: string;
   categoryKey: string;
   brand: string;
-  size: string;
+  size?: string;
   price: number;
-  purchaseDate: string;
+  purchaseDate?: string;
   initialUseCount: number;
   useCount: Readonly<number>;
   totalUseCount: Readonly<number>;
+  imageUrl: string;
 };
 
 export type ItemSummary = {
